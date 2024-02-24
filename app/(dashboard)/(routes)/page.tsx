a04f9aaf07b307dc12067f5beccdd0741490cd42
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2024-02-25 03:23:01 +0530

    added clerk authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
+import {UserButton} from '@clerk/nextjs';
 
 
 export default function Home() {
   return (
 <div>
- <p>Protectec 
-</p>
+<UserButton 
+ afterSignOutUrl='/'
+/>
 </div>
   
   );
