cdbe8e512e6a4ce012dbcbadd4b1be0bd8fba996
abhayyy30 <abhayyy.30@gmail.com>
2024-04-22 18:52:22 +0530

    update route.ts

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -7,8 +7,26 @@ export async function POST(req: Request) {
     const { userId } = auth();
     const { title } = await req.json();
 
+<<<<<<< Updated upstream
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
+=======
+        if(!userId){
+            return new NextResponse("Unauthorized",{status: 401}); 
+        }
+
+        const course = await db.course.create({
+            data:{
+                userId,
+                title,
+            }
+        });
+
+        return NextResponse.json(course);
+    }catch(error){
+        console.log("[COURSES]",error);
+        return new NextResponse("Internal Error",{status:500});
+>>>>>>> Stashed changes
     }
 
     const course = await db.course.create({
