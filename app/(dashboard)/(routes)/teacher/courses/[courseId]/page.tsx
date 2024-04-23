c6ade380009644dc23e57c47a2f4fd3a522b275d
prathamdasani <100029204+prathamdasani@users.noreply.github.com>
2024-04-23 15:05:08 +0530

    added chapter creation toggle

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { Chaptersform } from "./_components/chapters-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -21,9 +22,15 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+      userId
     },
     include: {
-      attachments:{
+      chapters: {
+        orderBy: {
+          position: "asc",
+        },
+      },
+      attachments: {
         orderBy: {
           createdAt: "desc",
         },
@@ -49,6 +56,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
@@ -88,9 +96,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               </h2>
 
             </div>
-            <div>
-              TODO: Chapters
-            </div>
+            <Chaptersform initialData={course} courseId={course.id} />
           </div>
           <div>
             <div className="flex items-center gap-x-2">
