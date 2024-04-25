81d865976d80cfe854baf8d3e1aea073acad72ac
abhayyy30 <abhayyy.30@gmail.com>
2024-04-25 16:32:14 +0530

    publish

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 import { db } from "@/lib/db";
 import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
-import { ChapterTitleForm } from "./components/chapter-title-form";
-import { ChapterDescriptionForm } from "./components/chapter-description-form";
-import { ChapterAccessForm } from "./components/chapter-access-form";
-import { ChapterVideoForm } from "./components/chapter-video-form";
+import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
   params,
@@ -42,7 +44,16 @@ const ChapterIdPage = async ({
 
   const completionText = `(${completedFields}/${totalFields})`;
 
+  const isComplete = requiredFields.every(Boolean);
+
   return (
+    <>
+    {!chapter.isPublished && (
+        <Banner 
+        variant="warning"
+        label="This chapter is unpublished. It will not be visible in the course"
+        />
+    )}
     <div className="p-6">
       <div className="flex items-center justify-between">
         <div className="w-full">
@@ -60,6 +71,12 @@ const ChapterIdPage = async ({
                 Complete all fields {completionText}
               </span>
             </div>
+            <ChapterActions
+            disabled={!isComplete}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+            isPublished={chapter.isPublished}
+            />
           </div>
         </div>
       </div>
@@ -106,6 +123,7 @@ const ChapterIdPage = async ({
         </div>
       </div>
     </div>
+    </>
   );
 };
 
